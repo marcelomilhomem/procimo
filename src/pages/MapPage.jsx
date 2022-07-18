0e6f4f8ebcc237a2217d0b576d72a00d11ef2516
Marcelo <marcelomilhomemm@hotmail.com>
2022-07-18 20:39:48 +0100

    markers networks working

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Div = styled.div`
   height: 80vh;
@@ -8,19 +10,47 @@ const Div = styled.div`
 `;
 
 function MapPage() {
+  const [networks, setNetworks] = useState([]);
+
+  const fetchApi = () => {
+    axios
+      .get(`http://api.citybik.es/v2/networks`, {})
+      .then((response) => {
+        setNetworks(response.data.networks);
+      })
+      .catch((err) => alert(err));
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: `${process.env.REACT_APP_API_KEY}`,
   });
+
   return (
-    
     <Div>
       {isLoaded ? (
         <GoogleMap
           mapContainerStyle={{ width: "100%", height: "100%" }}
           center={{ lat: 38.72726949547492, lng: -9.139262879074261 }}
           zoom={10}
-        ></GoogleMap>
+          disableDefaultUI
+        >
+          {networks.map((position) => {
+            console.log(position.location.latitude);
+            return (
+              <Marker
+                position={{
+                  lat: position.location.latitude,
+                  lng: position.location.longitude,
+                }}
+              />
+            );
+          })}
+        </GoogleMap>
       ) : (
         <></>
       )}
